e1d878a538dea86990005b7c9f2060b2afe3b27c
Harit Panchal <panchalharit007@gmail.com>
2022-08-10 18:10:32 +0530

    expiration time logic added

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -3,9 +3,18 @@ import React, { useState } from "react";
 const AuthContext = React.createContext({
   token: "",
   isLoggedIn: false,
-  login: (token) => {},
+  login: (token, expirationTime) => {},
   logout: () => {},
 });
+
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjustedExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingtime = adjustedExpirationTime - currentTime;
+
+  return remainingtime;
+};
 
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem("token");
@@ -13,14 +22,17 @@ export const AuthContextProvider = (props) => {
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token", token);
-  };
-
   const logutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+    setTimeout(logutHandler, remainingTime);
   };
 
   const tokenValue = {
